418e3ff3254b04c61634cd7eadbe263496de278e
Maciel Ribeiro de Oliveira <38005099+Maxnegrao33@users.noreply.github.com>
2022-09-11 10:22:03 -0300

    Add files via upload

---
+++ ComandosBásicos.js
@@ -0,0 +1,24 @@
+/*comentário no JavaScript é igual do Java e CSS*/
+/*Comando que exige uma mensagem de alerta no Console*/
+/*Toda mensagem tem que estar entre "aspas" pois trata-se de uma String*/
+
+alert("Iniciando uma mensagem");
+undefined
+
+/* Este comando exibe uma caixa de mensagem pedindo uma confirmação*/
+confirm("Deseja continuar estudando?");
+true
+confirm("Deseja continuar estudando?");
+false
+  
+/*Exibir uma mensagem na tela*/
+    console.log("Comandos Básicos");
+    console.log("Esta mensagem não aparece para o usuário")
+
+
+
+
+
+
+
+
